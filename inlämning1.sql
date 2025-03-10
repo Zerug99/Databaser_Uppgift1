0829252d0328b5cfcc88e4870b95922016d35bac
Patryk Jersak <108696082+Zerug99@users.noreply.github.com>
2025-03-10 16:15:30 +0100

    Update inlämning1.sql

--- inlämning1.sql
+++ inlämning1.sql
@@ -4,12 +4,12 @@ CREATE DATABASE pj_bokhandel;
 USE pj_bokhandel; 
 
 -- Nedanför ser man skapandet av en av tabellerna Kunder. Inom parenteserna anger man varje attribut med vilken datatyp som ska användas.
-CREATE TABLE kunder (  - Kunder är Entitet och nedanför anger man attributer 
+CREATE TABLE kunder (  -- Kunder är Entitet och nedanför anger man attributer 
 
-KundID INT AUTO_INCREMENT PRIMARY Key, -- skapar attributen och anger det som PK och automatisk utdelning av INT, alltså det delar ut en unikt siffra och man slipper göra det manuellt . PK står för primärnyckel och är som ett unikt identifierar för varje rad. 
+KundID INT AUTO_INCREMENT PRIMARY KEY, -- skapar attributen och anger det som PK och automatisk utdelning av INT, alltså det delar ut en unikt siffra och man slipper göra det manuellt . PK står för primärnyckel och är som ett unikt identifierar för varje rad. 
 
 
-Namn VARCHAR (255) NOT NULL, --  Varchar betyder att man kommer anger datatyp (tecken) och 255 är hur många tecken det får bli, NOT NULL betyder att namn måste alltid ha ett värde och får inte vara NOT NULL
+Namn VARCHAR (255) NOT NULL, --  VARCHAR betyder att man kommer anger datatyp (tecken) och 255 är hur många tecken det får bli, NOT NULL betyder att namn måste alltid ha ett värde och får inte vara NOT NULL
 
 E_POST VARCHAR (255) UNIQUE,-- UNIQUE betyder att en kund kan inte använda en existerande E_Post. Det ska vara unika. 
 
@@ -19,7 +19,7 @@ Adress VARCHAR (255) NOT NULL
 );
 
 
-CREATE TABLE böcker ( -- Här skapas en ny tabell och en ny datatyp här är DECIMAL, detta anger hur många siffror det får vara totalt - 10 och 2 står för hur många decimaler det får vara. (); Inom dessa skriver man koden och avlsutar på );
+CREATE TABLE böcker ( -- Här skapas en ny tabell och datatypen här är DECIMAL (10,2,) detta anger hur många siffror det får vara totalt 10 och 2 står för hur många decimaler det får vara. (  ); Inom dessa skriver man koden och avlsutar på );
 
 ISBN VARCHAR(255) PRIMARY KEY, 
 
@@ -50,7 +50,7 @@ FOREIGN KEY (KundID) REFERENCES Kunder(KundID) -- Här anger man En FK och pekar
 
 
 
-CREATE TABLE orderrader ( --Detta är vårt brygg - tabell som indirekt kopplar ihop beställningar och böcker tabellen ihop och är också kopplad till dem.  
+CREATE TABLE orderrader ( -- Detta är vårt brygg - tabell som indirekt kopplar ihop beställningar och böcker. Orderrader tabellen är också kopplad till dem.  
 
 OrderradID INT AUTO_INCREMENT PRIMARY KEY,
 
@@ -60,11 +60,20 @@ OrderID INT NOT NULL,
 
 Antal INT NOT NULL,
 
-FOREIGN KEY (ISBN) REFERENCES Böcker(ISBN), --Här kopplar vi igen som vi gjorde innan fast nu i böcker tabellen 
+FOREIGN KEY (ISBN) REFERENCES Böcker(ISBN), -- Här kopplar vi igen som vi gjorde innan fast nu i böcker tabellen 
 
 FOREIGN KEY (OrderID) REFERENCES beställningar(OrderID)
 );
 
+-- kod för att inserta value för att sedan kunna hämta upp den senare i rad 77
+
+INSERT INTO Kunder (Namn, E_Post, Telefon, Adress) VALUES -- Man srkiver först vilken tabell man vill lägga in datan och sedan vilka attributer som ska få sinna values.
+("Patryk Jersak", "patryk@test.com", 0123456789, "Algatan 46"); -- Viktigt att ha (); för att se början av radet och slutet. Man srkiver i ordning och använder " " mellan stringen. Symbolen , markerar slutet av värde 
 
 
 
+-- kod för att ta fram datan
+
+SELECT Telefon FROM Kunder WHERE Namn = "Patryk Jersak"; -- detta kommer hämta bara en specfikt information om angiven kund, bra när man behöver bara ett sorts data och inte belastar servern med att hämta all data.
+
+SELECT * FROM Kunder; -- detta kommer att prompta till att hämta (all) inforamtion från tabellen kunder.
